b2093c5a7657d2e261b53415f75708b99abd0c72
lukegre <lukegre@gmail.com>
2017-11-04 13:46:22 +0200

    new quenching depth

--- SeaGlider_utils.py
+++ SeaGlider_utils.py
@@ -10,13 +10,13 @@ For more info see help(SeaGlider)
 written by Luke Gregor (lukegre@gmail.com)
 """
 
-from __future__ import print_function
+from __future__ import print_function as _pf
 from netCDF4 import Dataset
 from pandas import DataFrame, Series
 import numpy as np
-import warnings
-warnings.filterwarnings("ignore", category=FutureWarning)
-warnings.filterwarnings("ignore", category=RuntimeWarning)
+import warnings as _warnings
+_warnings.filterwarnings("ignore", category=FutureWarning)
+_warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 
 class SeaGlider:
@@ -122,15 +122,18 @@ class _SeaGliderDiveVariable:
         self.__files__ = files
         self.name = name
         self.attrs = dict(nco.variables[name].__dict__)
-        self.__keys__ = [name]
         if 'coordinates' in self.attrs:
-            coordinates = self.attrs['coordinates'].split()
-            self.__keys__ += coordinates
+            coordinates = self.attrs['coordinates'].split() + ['dives']
             self.__data__.coordinates = coordinates
         elif not hasattr(self.__data__, 'coordinates'):
             self.__data__.coordinates = []
+        self.__data__.bins = np.arange(1000.)
 
         nco.close()
+
+    def __getitem__(self, key):
+        data = self.load(return_data=True)
+        return data.loc[key]
 
     @property
     def gridded(self):
@@ -145,37 +148,33 @@ class _SeaGliderDiveVariable:
 
     @property
     def series(self):
-        df = self.load(return_data=True)
-        df = df.set_index(self.__data__.time_name, drop=False)
-        return df.loc[:, self.name]
+        self.load()
+        return self.__data__.loc[:, self.name]
+
+    @property
+    def values(self):
+        self.load()
+        return self.__data__.loc[:, self.name].values
 
     def load(self, return_data=False):
-        for k in self.__data__.coordinates:
-            if k not in self.__keys__:
-                self.__keys__ += k,
-        keys = self.__keys__
+        # neaten up the script by creating labels
+        data = self.__data__
+        keys = np.unique(data.coordinates + [self.name])
+        files = self.__files__
 
-        missing_keys = np.array([k not in self.__data__ for k in keys])
+        # get keys not in dataframe
+        missing = [k for k in filter(lambda k: k not in data, keys)]
 
-        # if statement will load the data only if columns are missing
-        if any(missing_keys):
-            missing_keys = np.array(keys)[missing_keys]
-            fetch_dives = 'dives' not in self.__data__
-            df = self._read_nc_files(self.__files__, missing_keys, dives=fetch_dives)
-            # if variable has coordinates and not yet loaded, will be processed
-            if any([k in self.__data__.coordinates for k in missing_keys]):
-                df = self._process_coords(df, self.__files__[0])
-                if 'dives' in df:
-                    self.__data__.coordinates += 'dives',
-            # inplace assignment for the variables
+        if any(missing):
+            df = self._read_nc_files(files, missing)
+            # process coordinates - if no coordinates, just loops through
+            df = self._process_coords(df, files[0])
             for col in df:
-                self.__data__[col] = df[col]
-
-        if ('dives' in self.__data__.coordinates) & ('dives' not in self.__keys__):
-            self.__keys__ += 'dives',
+                # inplace column creation
+                data[col] = df[col]
 
         if return_data:
-            return self.__data__.loc[:, self.__keys__]
+            return data.loc[:, keys]
 
     def __repr__(self):
         string = ""
@@ -197,13 +196,23 @@ class _SeaGliderDiveVariable:
 
         return string
 
-    def _read_nc_files(self, files, keys, dives=True):
+    def _read_nc_files(self, files, keys):
         from tqdm import trange
+
+        if 'dives' in keys:
+            dives = True
+            keys.remove('dives')
+        else:
+            dives = False
 
         data = []
         for i in trange(files.size, ncols=80):
             fname = files[i]
             nc = Dataset(fname)
+            if any([k not in nc.variables for k in keys]):
+                message = "{} is missing some variables".format(fname)
+                print(message)
+                continue
             arr = np.r_[[nc.variables[k][:] for k in keys]]
             if dives:
                 arr = np.r_[arr, np.ones([1, arr.shape[1]]) * i]
@@ -252,7 +261,7 @@ class _SeaGliderDiveVariable:
 
         return df
 
-    def bin_depths(self, bins=None, how='mean', depth_name=None):
+    def bin_depths(self, bins=None, how='mean', depth=None):
         """
         This function bins the variable to set depths.
         If no depth is specified it defaults to:
@@ -278,19 +287,21 @@ class _SeaGliderDiveVariable:
 
         # load the data if this has not been done
         self.load()
+        data = self.__data__
 
-        if depth_name is None:
-            if hasattr(self.__data__, 'depth_name'):
-                depth = self.__data__.depth_name
+        if depth is None:
+            if hasattr(data, 'depth_name'):
+                depth = data.depth_name
             else:
-                print('please provide the name of the depth column')
+                print('please provide depth column name as a kwarg')
                 return None
 
-        if bins is None:
-            # this is the CSIR default that is chosen to save battery
-            bins = np.arange(0, 1000)
-        self.bins = bins
+        # assigning a bin value and make that the default for dimension
+        bins = data.bins if bins is None else bins
+        data.bins = bins
 
+        # The default bins only go to 1000m.
+        # Deepen with step same as last 10 values if needed
         if bins.max() < self.data[depth].max():
             step = mode(np.diff(bins[-10:])).mode
             start = bins[-1] + step
@@ -298,24 +309,25 @@ class _SeaGliderDiveVariable:
             extended_bins = np.arange(start, stop, step)
             bins = np.r_[bins, extended_bins]
 
-        labels = bins[:-1]
-        dives = self.data.dives
+        #
+        labels = (bins[:-1] + bins[1:]) / 2.
+        dives = self.data.dives.values
         bins = cut(self.data[depth], bins, labels=labels)
 
-        grp = self.data.groupby([dives, bins])
-        grp_agg = getattr(grp[self.name], how)()
+        grp_a = self.data.groupby([dives, bins])
+        grp_agg = getattr(grp_a[self.name], how)()
         gridded = grp_agg.unstack(level=0)
 
-        grp_cols = self.data[self.name].groupby(dives)
-        grp_idx0 = [grp_cols.groups[k][0] for k in grp_cols.groups]
-        if hasattr(self.__data__, 'time_name'):
-            gridded.columns = self.__data__.loc[grp_idx0, self.__data__.time_name]
-        else:
-            gridded.columns = self.data.loc[grp_idx0, 'dives']
+        if hasattr(data, 'time_name'):
+            grp_b = self.data[depth].groupby(dives)
+            idx = grp_b.apply(lambda s: s.index[np.nanargmin(s)])
+            gridded.columns = data.loc[idx, data.time_name]
+            gridded.columns.name = 'surface_time'
 
         if type(gridded.index) == core.indexes.category.CategoricalIndex:
             index = gridded.index.astype(float)
             gridded = gridded.set_index(index)
+            gridded.index.name = 'depth_bin_centre'
 
         self.__gridded__ = gridded
 
@@ -366,17 +378,19 @@ class _SeaGliderDiveVariable:
 
         return ax
 
-    def pcolormesh(self, interpolated=True, **kwargs):
+    def pcolormesh(self, interpolate_dist=0, **kwargs):
         """
         Plot a section plot of the dives with x-time and y-depth and
-        z-variable.
+        z-variable. The data can be linearly interpolated to fill missing
+        depth values. The number of points to interpolate can be set with
+        interpolate_dist.
         The **kwargs can be anything that gets passed to plt.pcolormesh.
         Note that the colour is scaled to 1 and 99% of z.
         """
         from matplotlib.pyplot import pcolormesh, colorbar, subplots
 
-        if interpolated:
-            gridded = self.gridded.interpolate(method='linear', limit=4)
+        if interpolate_dist:
+            gridded = self.gridded.interpolate(limit=interpolate_dist)
         else:
             gridded = self.gridded
 
@@ -532,6 +546,20 @@ class processing:
 
     def par_fill_surface(df, replace_depth=5, curve_max_depth=100):
         from scipy.optimize import curve_fit
+        """
+        Use an exponential fit to replace the surface values of PAR
+        and fill missing values with equation:
+            y(x) = a * exp(b * x)
+
+        INPUT:  df - a dp.DataFrame indexed by depth
+                   - can also be group item grouped by dive, indexed by depth
+                replace_depth [5] - from replace_depth to surface is replaced
+                curve_depth [100] - the depth from which the curve is fit
+        OUPUT:  a pd.Series with the top values replaced.
+
+        Note that this function is a wrapper around a function
+        that is applied to the gridded dataframe or groups
+        """
 
         def fit_exp_curve(ser, replace_depth, curve_max_depth):
 
@@ -554,33 +582,165 @@ class processing:
             ser.loc[j] = y_hat[j]
 
             return ser
-        """
-        Use an exponential fit to replace the surface values of PAR:
-            Equation: y(x) = a * exp(b * x)
 
-        INPUT:  df - a dp.DataFrame indexed by depth
-                   - can also be group item grouped by dive, indexed by depth
-                replace_depth [5] - from replace_depth to surface is replaced
-                curve_depth [100] - the depth from which the curve is fit
-        OUPUT:  a pd.Series with the top values replaced.
-
-        Note that this function is a wrapper around a function
-        that is applied to the gridded dataframe or groups
-        """
+        input_args = (replace_depth, curve_max_depth)
         cols = df.notnull().sum() > 3
         df_sub = df.loc[:, cols]
-        df_sub = df_sub.apply(fit_exp_curve, args=(replace_depth, curve_max_depth))
-        df.loc[:, cols] = df_sub
+        df_sub = df_sub.apply(fit_exp_curve, args=input_args)
+        df.loc[:, cols] = df_sub.values
 
         return df
 
-    def flr_quenching(chl, ):
-        pass
+    def photic_depth(par, dark_lim_uE=10, return_depth_index=False):
+        """
+        Nothing fancy, just finds the 1% depth of light for each profile.
+        It does not do this on dark profiles where the top 10 meters have
+        a mean light of less than 10 µE.
+        """
+
+        dark = par.iloc[par.index < 10].mean() < dark_lim_uE
+        par_1pct = par.iloc[0] * 0.01
+        par_1pct.loc[dark] = np.NaN
+
+        euph_diff = (par - par_1pct) >= 0
+        euph_csum = (~euph_diff).cumsum()
+        euph_layer = euph_csum == 0
+
+        if return_depth_index:
+            euph_depth = par.index[-1] - euph_csum.iloc[-1]
+            euph_depth.loc[euph_depth < 0] = np.NaN
+
+            return euph_layer, euph_depth
+
+        else:
+            return euph_layer
+
+    def sunset_sunrise(time, lat, lon):
+        """
+        Uses the Astral package to find sunset and sunrise times.
+        The times are returned rather than day or night indicies.
+        More flexible for quenching corrections.
+        """
+        from astral import Astral
+        ast = Astral()
+
+        df = DataFrame.from_items([
+            ('time', time),
+            ('lat', lat),
+            ('lon', lon),
+        ])
+        # set days as index
+        df = df.set_index(df.time.values.astype('datetime64[D]'))
+
+        # groupby days and find sunrise for unique days
+        grp = df.groupby(df.index).mean()
+        date = grp.index.to_pydatetime()
+
+        grp['sunrise'] = list(map(ast.sunrise_utc, date, df.lat, df.lon))
+        grp['sunset'] = list(map(ast.sunset_utc, date, df.lat, df.lon))
+
+        # reindex days to original dataframe as night
+        df[['sunrise', 'sunset']] = grp[['sunrise', 'sunset']].reindex(df.index)
+
+        # set time as index again
+        df = df.set_index('time', drop=False)
+        cols = ['time', 'sunset', 'sunrise']
+        return df[cols]
+
+    def layer_bottom_depth(layer):
+        layer_ind = layer.diff().astype(float)
+        layer_bottom = layer_ind.apply(lambda s: s.index[np.nanargmax(s) - 1])
+        layer_bottom.loc[layer_bottom == layer.index[0]] = np.nan
+
+        return layer_bottom
+
+    def quenching_depth(fluo, lat, lon, photic_layer, night_day_group=True, ):
+        """
+        Calculates the fluorescence depth.
+        INPUT:  fluo - pd.DataFrame(idx=depth, col=surface_time), despiked
+                lat  - pd.DataFrame(idx=depth, col=surface_time)
+                lon  - pd.DataFrame(idx=depth, col=surface_time)
+                photic_layer - pd.DataFrame(idx=depth, col=surface_time)
+                night_day_group=True - quenching corrected with preceding night
+                                False - quenching corrected with following night
+
+        OUTPUT: quenching depth (pd.Series) indexed by surface_time
+
+        """
+        def quench_nmin_grad(diff_ser, roll_window=4, skip_n_meters=5):
+            """
+            Quenching depth for a day/night fluorescence difference
+
+            INPUT:   pandas.Series indexed by depth
+                     roll_window [4] is a rolling window size to remove spikes
+                     skip_n_meters [5] skips the top layer that is often 0
+            OUPUT:   estimated quenching depth as a float or int
+                     note that this can be NaN if no fluorescence measurement
+                     OR if the average difference is less than 0
+            """
+
+            # When the average is NaN or less than 0 don't give a depth
+            # Average difference of < 0 is an artefact
+            if not (diff_ser.mean() > 0):
+                return np.NaN
+
+            # The rolling window removes spikes creating fake shallow QD
+            x = diff_ser.rolling(roll_window, center=True).mean()
+            # We also skip the first N meters as fluorescence is often 0
+            x_abs = x.iloc[skip_n_meters:].abs()
+
+            # 5 smallest absolute differences included
+            x_small = x_abs.nsmallest(5)
+            # Points that cross the 0 difference and make nans False
+            sign_change = (x > 0).diff(1)
+            sign_change.iloc[[0, -1]] = False
+            x_sign_change = x_abs[sign_change]
+            # subset of x to run gradient on
+            x_subs = x_small.append(x_sign_change)
+
+            # find the steepest gradient from largest difference
+            x_ref = x_subs - x.iloc[:roll_window + skip_n_meters].max()
+            x_grad = x_ref / x_ref.index.values
+            # index of the largest negative gradient
+            x_grad_min = x_grad.idxmin()
+
+            return x_grad_min
+
+        night_day_group = True
+
+        # get the coordinates of the top 20 meters of the dives
+        surf_lat = lat.loc[:20].mean()
+        surf_lon = lon.loc[:20].mean()
+        surf_time = fluo.columns.values
+
+        # get the sunrise sunset times
+        sun = processing.sunset_sunrise(surf_time, surf_lat, surf_lon)
+        # calculate day night times
+        day = (sun.time > sun.sunrise) & (sun.time < sun.sunset)
+
+        # creating quenching correction batches, where a batch is a
+        # night and the following day
+        if type(night_day_group) is not bool:
+            raise TypeError("`night_day_group` must be boolean.")
+        batch = (day.diff().cumsum() + night_day_group) // 2
+        batch[0] = 0
+
+        # Group the fluorescence by daytime and quenching batch
+        grouped = fluo.groupby([day.values, batch.values], axis=1)
+        fluo_night_median = grouped.median()[False]  # get the night values
+
+        # Calculate the nighttime fluorescence and extrapolate this to day
+        # so that the difference between night and day can be calculated
+        fluo_night = fluo_night_median.reindex(columns=batch.values)
+        fluo_night.columns = fluo.columns
+
+        # find the depth at which mean-nighttime and daytime fluorescence cross
+        diff = (fluo_night - fluo).where(photic_layer)
+        quench_depth = quench_nmin_grad(diff, window=4, skip_n_meters=5)
+
+        return quench_depth
 
     def flr_dark_count(df):
-        pass
-
-    def par_dark_count(df):
         pass
 
     def bks_scaling(bb):
@@ -606,6 +766,42 @@ class processing:
 
     def despike_running_mean(df):
         pass
+
+    def bin_depths(ser, depth, dives, bins, how='mean', depth_name=None):
+        """
+        This function bins the variable to set depths.
+        If no depth is specified it defaults to:
+            start : step : end
+            ------------------
+                0 :  0.5 : 100
+              100 :  1.0 : 400
+              400 :  2.0 : max
+            ------------------
+        This is the sampling method typically used by the CSIR.
+        This can be easily changed by specifying an array of increasing values.
+
+        The function used to bin the data can also be set, where the default
+        is the mean. This can be changed to 'median', 'std', 'count', etc...
+
+        Lastly, the data is stored as SeaGliderVariable.gridded for future
+        easy access. If you would like to regrid the data to another grid
+        use SeaGliderVariable.bin_depths().
+        """
+        from pandas import cut, core
+
+        if type(depth) is core.series.Series:
+            depth = depth.values
+        if type(dives) is core.series.Series:
+            dives = dives.values
+
+        labels = bins[:-1]
+        bins = cut(depth, bins, labels=labels)
+
+        grp = ser.groupby([dives, bins])
+        grp_agg = getattr(grp, how)()
+        gridded = grp_agg.unstack(level=0)
+
+        return gridded
 
 
 if __name__ == '__main__':
